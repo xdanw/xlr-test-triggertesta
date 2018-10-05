6cd905ffc8249f7b4306212625613480e0684583
xdanw <xdanw>
2018-10-05 00:41:43 -0600

    Update readme and comments

--- build/resources/main/testScript/testTriggerA.py
+++ build/resources/main/testScript/testTriggerA.py
@@ -8,9 +8,8 @@ if triggerState == "":
     triggerState = "0";
     print "set > 0";
 
-else:
-    if int(triggerState >= 1):
-        triggerState = str(int(triggerState)+1);
-        print "set > " + str(int(triggerState)) + "+1=" + str(int(triggerState)+1);
+if int(triggerState >= 0):
+    triggerState = str(int(triggerState)+1);
+    print "set > " + str(int(triggerState)) + "+1=" + str(int(triggerState)+1);
 
 print "triggerState_post: " + triggerState;
--- src/main/resources/testScript/testTriggerA.py
+++ src/main/resources/testScript/testTriggerA.py
@@ -8,9 +8,9 @@ if triggerState == "":
     triggerState = "0";
     print "set > 0";
 
-else:
-    if int(triggerState >= 1):
-        triggerState = str(int(triggerState)+1);
-        print "set > " + str(int(triggerState)) + "+1=" + str(int(triggerState)+1);
+# else: ... nah, for testing let's increment to 1 on the first run
+if int(triggerState >= 0):
+    triggerState = str(int(triggerState)+1);
+    print "set > " + str(int(triggerState)) + "+1=" + str(int(triggerState)+1);
 
 print "triggerState_post: " + triggerState;
